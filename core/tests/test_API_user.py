0088f68b86d03cb91d7f3ce1873bcd453f0fd006
Johnny Fang <fangdejavu@gmail.com>
2019-04-23 19:50:18 -0500

    code cleanup

--- core/tests/test_API_user.py
+++ core/tests/test_API_user.py
@@ -1,7 +1,6 @@
 import pytest
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from mixer.backend.django import mixer
 from django.urls import reverse
 
 from rest_framework.test import APIClient
